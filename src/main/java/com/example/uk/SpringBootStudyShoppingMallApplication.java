e3a3fbbb5f03dc8f025f7cc68f960870b22ef353
UK <uk_kdw8794@naver.com>
2023-06-03 16:51:24 +0900

    commit

---
+++ src/main/java/com/example/uk/SpringBootStudyShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.uk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudyShoppingMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudyShoppingMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uk/SpringBootStudyShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyShoppingMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
