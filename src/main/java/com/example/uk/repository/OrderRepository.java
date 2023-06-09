c05bb1027d3c7040285c5fe22a3983738347df05
UK <uk_kdw8794@naver.com>
2023-06-09 15:33:07 +0900

    commit

--- src/main/java/com/example/uk/repository/OrderRepository.java
+++ src/main/java/com/example/uk/repository/OrderRepository.java
@@ -14,10 +14,10 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
             "WHERE o.member.email = :email " +
             "ORDER BY o.orderDate DESC"
     )
-    List<Order> findOrders(@Param("email") String email, Pageable pageable);
+    List<Order> findOrders(@Param("email") String email, Pageable pageable);    // 현재 로그인한 사용자의 주문 데이터를 페이징 조건에 맞춰서 조회
 
     @Query("SELECT COUNT(o) FROM Order o " +
             "WHERE o.member.email = :email"
     )
-    Long countOrder(@Param("email") String email);
+    Long countOrder(@Param("email") String email);  // 현재 로그인한 회원의 주문 갯수가 몇개인지 조회
 }
--- src/main/java/com/example/uk/service/OrderService.java
+++ src/main/java/com/example/uk/service/OrderService.java
@@ -1,15 +1,18 @@
 package com.example.uk.service;
 
 import com.example.uk.dto.OrderDto;
-import com.example.uk.entity.Item;
-import com.example.uk.entity.Member;
-import com.example.uk.entity.Order;
-import com.example.uk.entity.OrderItem;
+import com.example.uk.dto.OrderHistDto;
+import com.example.uk.dto.OrderItemDto;
+import com.example.uk.entity.*;
+import com.example.uk.repository.ItemImgRepository;
 import com.example.uk.repository.ItemRepository;
 import com.example.uk.repository.MemberRepository;
 import com.example.uk.repository.OrderRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +27,7 @@ public class OrderService {
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
+    private final ItemImgRepository itemImgRepository;
 
     public Long order(OrderDto orderDto, String email) {
         Item item = itemRepository.findById(orderDto.getItemId()).orElseThrow(EntityNotFoundException::new);
@@ -37,5 +41,29 @@ public class OrderService {
         orderRepository.save(order);
 
         return order.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<OrderHistDto> getOrderList(String email, Pageable pageable) {
+
+        List<Order> orders = orderRepository.findOrders(email, pageable);   // 유저의 아이디와 페이징 조건을 이용하여 주문 목록을 조회
+        Long totalCount = orderRepository.countOrder(email);                // 유저의 주문 총 개수를 구함
+
+        List<OrderHistDto> orderHistDtos = new ArrayList<>();
+
+        for (Order order : orders) {    // 주문 리스트를 순회하면서 구매 이력 페이지에 전달할 DTO 를 생성
+            OrderHistDto orderHistDto = new OrderHistDto(order);
+            List<OrderItem> orderItems = order.getOrderItems();
+
+            for (OrderItem orderItem : orderItems) {
+                ItemImg itemImg = itemImgRepository.findByItemIdAndRepimgYn(orderItem.getItem().getId(), "Y");  // 주문한 상품의 대표 이미지를 조회
+                OrderItemDto orderItemDto = new OrderItemDto(orderItem, itemImg.getImgUrl());
+                orderHistDto.addOrderItemDto(orderItemDto);
+            }
+
+            orderHistDtos.add(orderHistDto);
+        }
+
+        return new PageImpl<OrderHistDto>(orderHistDtos, pageable, totalCount); // 페이지 구현 객체를 생성하여 반환
     }
 }
